847bcb989491281a16ad3014be4486fb991e8f5a
Harsh180303 <harshchourasiya6261@gmail.com>
2025-07-19 22:18:58 +0530

    day 1

---
+++ Day-1/printExercise.py
@@ -0,0 +1,7 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("Hello\nHello\nHello")
+
+print("Hello " + "Harsh")
---
+++ Day-1/variable.py
@@ -0,0 +1,9 @@
+name = 'Harsh'
+print(name)
+
+name = "Chotu"
+print(name)
+
+name = input("What is your name?")
+length = len(name)
+print('The length of your name is : ' + length)
