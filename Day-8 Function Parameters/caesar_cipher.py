5371c67dbea2b25a020fa2088f1d413f18741bf4
Harsh180303 <harshchourasiya6261@gmail.com>
2025-07-25 16:34:03 +0530

    made my code modular

--- Day-8 Function Parameters/caesar_cipher.py
+++ Day-8 Function Parameters/caesar_cipher.py
@@ -1,30 +1,20 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-def encrypt(plain_text, shift_amount) :
-    cipher_text = ''
-    for letter in plain_text:
-        positon = alphabet.index(letter)
-        new_position = positon + shift_amount
-        cipher_text += alphabet[new_position]
-    print(f"Your encoded text is {cipher_text}")
-
-def decrypt(cipher_text, shift_amount):
-    plain_text = ''
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        plain_text += alphabet[new_position]
-    print(f"Your decoded text is {plain_text}")
-
     
+def caesar(cipher_direction, start_text, shift_amount) :
+    end_text = ""
+    for letter in start_text:
+        position = alphabet.index(letter)
+        if cipher_direction == "encode".lower() :
+            new_position = position + shift_amount
+        elif cipher_direction == "decode".lower() :
+            new_position = position - shift_amount
+    
+        end_text += alphabet[new_position]
+    
+    print(end_text)
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-if direction == "encode".lower() :
-    encrypt(text, shift)
-elif direction == "decode".lower() :
-    decrypt(text, shift)
-else :
-    print("Please choose among encode or decode only nothing else.")
-    
+caesar(direction, text, shift)
