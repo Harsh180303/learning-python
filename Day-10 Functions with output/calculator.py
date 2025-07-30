cd60d3552d3ec8800c2dc1b62f3d6707d3461b71
Harsh180303 <harshchourasiya6261@gmail.com>
2025-07-30 13:02:28 +0530

    improved logic

--- Day-10 Functions with output/calculator.py
+++ Day-10 Functions with output/calculator.py
@@ -21,20 +21,25 @@ operations = {
     "/": divide,
 }
 
-num1 = float(input("Enter your first number: "))
-finished = False
-while not finished :
+def calculator() :
+    num1 = float(input("Enter your first number: "))
     for symbol in operations:
         print(symbol)
-    operation_symbol = input(f"Enter the operation do you want to perform: ")
-    num2 = float(input("Enter your second number: "))
 
-    calculation_function = operations[operation_symbol]
-    first_answer = calculation_function(num1, num2)
+    finished = False
+    while not finished :
+        operation_symbol = input(f"Enter the operation do you want to perform: ")
+        num2 = float(input("Enter your next number: "))
 
-    print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-    should_continue = input(f"Type 'y' to continue with {first_answer}, or type 'n' to exit.: ")
-    if should_continue == 'n' :
-        finished = True
-    if should_continue == 'y':
-        num1 = first_answer
+        calculation_function = operations[operation_symbol]
+        first_answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+        should_continue = input(f"Type 'y' to continue with {first_answer}, or type 'n' to start new calculation.: ")
+        if should_continue == 'n' :
+            finished = True
+            calculator()
+        if should_continue == 'y':
+            num1 = first_answer
+
+calculator()
