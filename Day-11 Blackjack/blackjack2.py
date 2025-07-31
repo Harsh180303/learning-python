331b909e5a3f3de6ea58921233225cb6ee57d499
Harsh180303 <harshchourasiya6261@gmail.com>
2025-07-31 17:47:59 +0530

    added calculate score functionality

--- Day-11 Blackjack/blackjack2.py
+++ Day-11 Blackjack/blackjack2.py
@@ -12,5 +12,16 @@ for _ in range(2) :
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-print(user_cards)
-print(computer_cards)
+def calculate_score(cards) :
+    if sum(cards) == 21 and len(cards) == 2 :
+        return 0
+    
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+        
+    return sum(cards)
+
+print(f"Your cards: {user_cards}, current score: {calculate_score(user_cards)}")
+
+print(f"Computer's cards: {computer_cards}, computer's score: {calculate_score(computer_cards)}")
