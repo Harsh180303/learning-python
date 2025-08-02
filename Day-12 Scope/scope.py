46cab6e53135582c9cac673100dd808277a5cf6b
Harsh180303 <harshchourasiya6261@gmail.com>
2025-08-02 18:37:24 +0530

    use of return statement in python

--- Day-12 Scope/scope.py
+++ Day-12 Scope/scope.py
@@ -1,10 +1,27 @@
 # Example
 
+
+# In this way you can access enemies which is outside the function but not recommended
 enemies = 1
 
 def increase_enemies() :
-    enemies = 2
+    global enemies
+    print(f"Enemies insied function before increasing: {enemies}")
+    enemies += 1
     print(f"Enemies insied function: {enemies}")
 
 increase_enemies()
+print(f"Enemies outside function: {enemies}")
+
+
+# Instead of doing this (above) you can return the modifies value
+
+enemies = 1
+
+def increase_enemies() :
+    print(f"Enemies insied function before increasing: {enemies}")
+    return enemies + 1
+    # print(f"Enemies insied function: {enemies}")
+
+enemies = increase_enemies()
 print(f"Enemies outside function: {enemies}")
