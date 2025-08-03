e4ec10e33f21cfb43388d2f30929cfb9ed1ec466
Harsh180303 <harshchourasiya6261@gmail.com>
2025-08-03 23:11:52 +0530

    initial commit of project- number guessing game

---
+++ Day-12 Scope/globalConstants.py
@@ -0,0 +1,8 @@
+# Global Constants (goloab scope can be incredibly useful espacially when you're defining constants.)
+
+PI = 3.14159
+
+def calc():
+    print(PI)
+
+calc()
---
+++ Day-12 Scope/numberGuessingGame.py
@@ -0,0 +1,5 @@
+# Welcome
+print("Welcme to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+difficulty = print(input("Choose a difficulty. Type 'easy' or 'hard':"))
