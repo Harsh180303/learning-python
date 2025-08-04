b0fc9c632f6256a749dfe7dcc2f7ead4e7b14246
Harsh180303 <harshchourasiya6261@gmail.com>
2025-08-04 13:50:21 +0530

    built number guessing game

--- Day-12 Scope/numberGuessingGame.py
+++ Day-12 Scope/numberGuessingGame.py
@@ -1,5 +1,34 @@
+import random
+
 # Welcome
-print("Welcme to the Number Guessing Game!")
+print("Welcome to the Number Guessing Game!")
+answer = random.randint(1, 100)
 print("I'm thinking of a number between 1 and 100.")
 
-difficulty = print(input("Choose a difficulty. Type 'easy' or 'hard':"))
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard':").lower()
+
+HARD_LEVEL_TURNS = 5
+EASY_LEVEL_TURNS = 10
+
+should_continue = True
+
+def guess_the_num(turns) :
+    while (turns > 0):
+        print(f"You have {turns} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        if guess == answer:
+            print(f"You got it! The answer was {answer}.")
+            should_continue = False
+        elif guess > answer:
+            print("Too high.")
+        else:
+            print("Too low.")
+        turns -= 1
+
+    print(f"You couldn't get the answer, The correct answer was {answer}")
+
+
+if (difficulty == 'easy'):
+    guess_the_num(EASY_LEVEL_TURNS)
+else :
+    guess_the_num(HARD_LEVEL_TURNS)
