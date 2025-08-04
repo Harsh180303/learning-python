614532c524559003d8b21e2308df1b99e8a2296c
Harsh180303 <harshchourasiya6261@gmail.com>
2025-08-04 16:41:32 +0530

    removed unused variable

--- Day-12 Scope/numberGuessingGame.py
+++ Day-12 Scope/numberGuessingGame.py
@@ -1,26 +1,15 @@
 import random
 
-# Welcome
-print("Welcome to the Number Guessing Game!")
-answer = random.randint(1, 100)
-print("I'm thinking of a number between 1 and 100.")
-# print(f"for testing {answer}")
-
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard':").lower()
-
 HARD_LEVEL_TURNS = 5
 EASY_LEVEL_TURNS = 10
 
-should_continue = True
-
-def guess_the_num(turns) :
+def guess_the_num(turns, answer) :
     while (turns > 0):
         print(f"You have {turns} attempts remaining to guess the number.")
         guess = int(input("Make a guess: "))
         if guess == answer:
             print(f"You got it! The answer was {answer}.")
             return
-            should_continue = False
         elif guess > answer:
             print("Too high.")
         else:
@@ -29,8 +18,15 @@ def guess_the_num(turns) :
 
     print(f"You couldn't get the answer, The correct answer was {answer}")
 
+# Welcome
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+answer = random.randint(1, 100)
+# print(f"for testing {answer}")
+
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 
 if (difficulty == 'easy'):
-    guess_the_num(EASY_LEVEL_TURNS)
+    guess_the_num(EASY_LEVEL_TURNS, answer)
 else :
-    guess_the_num(HARD_LEVEL_TURNS)
+    guess_the_num(HARD_LEVEL_TURNS, answer)
