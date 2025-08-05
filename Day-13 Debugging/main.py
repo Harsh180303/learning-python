f0b96bfa3e3319453359fec5bbcab817fc80feee
Harsh180303 <harshchourasiya6261@gmail.com>
2025-08-05 23:26:12 +0530

    debug problem 2

--- Day-13 Debugging/main.py
+++ Day-13 Debugging/main.py
@@ -1,7 +1,22 @@
+# Problem 1
 # range does not include the last digit
 
+"""
 def my_function():
-    for i in range(1, 20):
+    # for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 my_function()
+"""
+
+# Problem 2
+
+"""
+import random
+dice_num = ["1", "2", "3", "4", "5", "6"]
+# dice_index = random.randint(1, 6)
+dice_index = random.randint(0, 5)
+print(dice_num[dice_index])
+"""
+
