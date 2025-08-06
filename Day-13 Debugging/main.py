952cbec94787becea260b1e5e1f126ce81f44c7c
Harsh180303 <harshchourasiya6261@gmail.com>
2025-08-06 17:34:37 +0530

    debug the poblems

--- Day-13 Debugging/main.py
+++ Day-13 Debugging/main.py
@@ -20,3 +20,50 @@ dice_index = random.randint(0, 5)
 print(dice_num[dice_index])
 """
 
+# Problem 3
+"""
+# play Computer
+year = int(input("What is your year of birth? "))
+# if year > 1980 and year < 1994:
+if year > 1980 and year <= 1994:
+    print("You are a millenial.")
+elif year > 1994:
+    print("You are a Gen Z.")
+"""
+
+# Problem 4
+"""
+# Fix the Errors
+age = int(input("How old are you? "))
+if age > 18:
+    print(f"You can drive at age {age}.")
+"""
+
+# Problem 5
+"""
+# Print is Your Friend
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(f"Pages = {pages}")
+print(f"Words per page = {word_per_page}")
+print(total_words)
+"""
+
+# Problem 6
+"""
+# Use a Debugger
+def mutete(a_list) :
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+    # b_list.append(new_item)  # this should be inside the loop
+        b_list.append(new_item)  # like this
+    print(b_list)
+
+mutete([1,2,3,5,8,13])
+"""
+
