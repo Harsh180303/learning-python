ad9bce11b286c36ea43080a0872e114c3e379134
Harsh180303 <harshchourasiya6261@gmail.com>
2025-08-06 22:12:33 +0530

    Debug problem 8

--- Day-13 Debugging/main.py
+++ Day-13 Debugging/main.py
@@ -67,3 +67,29 @@ def mutete(a_list) :
 mutete([1,2,3,5,8,13])
 """
 
+# Problem 7
+"""
+number = int(input("Which number do you want to check? "))
+# if number % 2 = 0: # here we are assigning 0 which is wrong
+if number % 2 == 0:
+    print("This is an even number.")
+else :
+    print("This is an odd number.")
+"""
+
+# Problem 8
+# Leap year Exercise
+
+# year = input("Which year do you want to check? ")  # input is a string here....
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else :
+        print("Leap year.")
+else :
+    print("Not leap year.")
