7dbc6717ddb03139e3ab43e5ca1b3c60a793dbb7
Harsh180303 <harshchourasiya6261@gmail.com>
2025-08-08 23:18:15 +0530

    Built higher and lower game.

--- Day-14 higher_lower_game/game.py
+++ Day-14 higher_lower_game/game.py
@@ -1,5 +1,5 @@
-from art import logo
-# print(logo)
+from art import logo, vs
+print(logo)
 from game_data import data
 import random
 
@@ -8,19 +8,26 @@ def getRandomUser() :
     return user
 
 user1 = getRandomUser()
-user2 = getRandomUser()
-print(f"Compare A: {user1["name"]}, a {user1["description"]} from {user1["country"]}.")
-print(f"Against B: {user2["name"]}, a {user2["description"]} from {user2["country"]}.")
-
-
-choice = input("Who has more followers? Type 'A' or 'B': ").upper()
 score = 0
-if choice == 'A' and user1["follower_count"] > user2["follower_count"] :
-    score += 1
-    print(f"You're right! Current score: {score}")
-elif choice == 'B' and user2["follower_count"] > user1["follower_count"] :
-    score += 1
-    print(f"You're right! Current score: {score}")
-else :
-    score = 0
-    print(f"Sorry, that's wrong. Final score: {score}")
+win = True
+while (win) :
+    user2 = getRandomUser()
+    while user2 == user1:
+        user2 = getRandomUser()
+    print(f"Compare A: {user1['name']}, a {user1['description']} from {user1['country']}.")
+    print(vs)
+    print(f"Against B: {user2['name']}, a {user2['description']} from {user2['country']}.")
+    choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+    if choice == 'A' and user1['follower_count'] > user2['follower_count'] :
+        score += 1
+        print(f"You're right! Current score: {score}")
+        user1 = user2
+
+    elif choice == 'B' and user2['follower_count'] > user1['follower_count'] :
+        score += 1
+        print(f"You're right! Current score: {score}")
+        # user1 = user1
+    else :
+        score = 0
+        print(f"Sorry, that's wrong. Final score: {score}")
+        win = False
