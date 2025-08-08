0c475b911723dd02eafffc51e2933404b1069226
Harsh180303 <harshchourasiya6261@gmail.com>
2025-08-08 20:08:11 +0530

    build some logic

--- Day-14 higher_lower_game/game.py
+++ Day-14 higher_lower_game/game.py
@@ -9,10 +9,18 @@ def getRandomUser() :
 
 user1 = getRandomUser()
 user2 = getRandomUser()
-print(f"Compare A: {user1["name"]}, a {user1["description"]} from {user1["country"]}")
-print(f"Against B: {user2["name"]}, a {user2["description"]} from {user2["country"]}")
+print(f"Compare A: {user1["name"]}, a {user1["description"]} from {user1["country"]}.")
+print(f"Against B: {user2["name"]}, a {user2["description"]} from {user2["country"]}.")
 
 
 choice = input("Who has more followers? Type 'A' or 'B': ").upper()
-
-# print(getRandomUser())
+score = 0
+if choice == 'A' and user1["follower_count"] > user2["follower_count"] :
+    score += 1
+    print(f"You're right! Current score: {score}")
+elif choice == 'B' and user2["follower_count"] > user1["follower_count"] :
+    score += 1
+    print(f"You're right! Current score: {score}")
+else :
+    score = 0
+    print(f"Sorry, that's wrong. Final score: {score}")
