6757a17da9cdd448135240cc57915d7b27f8c7f7
Harsh180303 <harshchourasiya6261@gmail.com>
2025-08-19 23:31:46 +0530

    make it usable

--- Day-17 Quiz Project/Quiz/question_model.py
+++ Day-17 Quiz Project/Quiz/question_model.py
@@ -2,6 +2,3 @@ class Question :
     def __init__(self, q_text, q_answer):
         self.text = q_text
         self.answer = q_answer
-
-new_q = Question("question no 1 ", "False")
-print(new_q.text)
